28cd2aec27e004f1c5e0c5a02bc617dc7344af78
Gerson Arce <arceger@hotmail.com>
2024-03-08 16:44:17 +0000

    routes

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -1,12 +1,30 @@
 package com.test;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import over.ListenerService;
 import over.ListenerServiceImpl;
-
+@SpringBootApplication
+@RestController
 public class Main {
+
+    @GetMapping("/")
+    public String bemvindo() {
+        return "aplicacao no ar";
+    }
+    @GetMapping("/{input}")
+    public String congratu(@PathVariable String input){
+        return "bem vindo ao openshift ..";
+    }
+
 
 
     public static void main(String[] args) {
+
+        SpringApplication.run(Main.class, args);
 
    /*     ListenerService listenerService = new ListenerServiceImpl();
 
